7685f43e7fa1e31ecc7240d08f3bdc7d48380aea
Mateus Lemos <lemonsmateus@gmail.com>
2022-07-03 21:20:02 -0300

    Add files via upload

--- ula.v
+++ ula.v
@@ -1,16 +1,24 @@
-module ULA (a, b, c, result, zero, ULAOp)
+module ULA (a, b, c, result, zero, ULAOp);
 
 clock C1 (c);
-input reg a, b, c;
-unsigned reg leftOp, rightOp;
-input reg [1:0] ULAOp;
-output reg result;
+input a, b;
+input c;
+reg [7:0] leftOp, rightOp;
+input [1:0] ULAOp;
+output reg [7:0] result;
 output zero;
+
+assign zero = (result==0);
+assign ULAOp = 2'b00;
 
 initial begin
 
-leftOp = a;
-rightOp = b;
+//apenas para fins de simulacao
+
+leftOp = 8'b00000100;
+rightOp = 8'b00000001;
+
+$monitor(" %0d %b %b %b ", $time, leftOp, rightOp, result);
 
 end
 
@@ -18,10 +26,11 @@ always @(posedge c) begin
 
 	case (ULAOp)
 	
-	2'b00: result = a + b;
-	2'b01:	result = a - b;
-	2'b10:	result = a . b;
-	2'b11:	result = a & b;
+	2'b00: result = leftOp + rightOp;
+	2'b01:	result = leftOp - rightOp;
+	2'b10:	result = leftOp && rightOp;
+	2'b11:	result = leftOp || rightOp;
+	default: result = 0; //esse caso nao deve acontecer
 
 	endcase
 
